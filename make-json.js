4ead672ad8398ad44e2b54735427c574a05fd724
Keith C. Ivey <keith@iveys.org>
2015-06-08 22:20:47 -0400

    add make-json.js

---
+++ make-json.js
@@ -0,0 +1,15 @@
+var fs = require('fs'),
+    yaml = require('js-yaml'),
+    personDir = __dirname + '/data/person',
+    file = __dirname + '/data/person.json',
+    personData = {};
+
+fs.readdirSync(personDir).forEach(function (file) {
+    if (!/\.yaml$/.test(file)) {
+        return;
+    }
+    var d = yaml.safeLoad(fs.readFileSync(personDir + '/' + file));
+    personData[d.code] = d;
+});
+
+fs.writeFileSync(file, JSON.stringify(personData));
