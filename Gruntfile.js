b11ca44c48604f4493286e342624bafb58645903
Keith C. Ivey <keith@iveys.org>
2019-01-03 13:09:58 -0500

    add grunt-bump

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+var files = ['package.json', 'package-lock.json'];
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        bump: {
+            options: {
+                commitFiles: files,
+                files: files,
+                push: false
+            }
+        }
+    });
+    require('load-grunt-tasks')(grunt);
+};
