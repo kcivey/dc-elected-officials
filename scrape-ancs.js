315de067688a8ac7a3006f8270a1eb7a62d2a11d
Keith C. Ivey <keith@iveys.org>
2019-04-04 11:58:08 -0400

    parse ANC tables and make CSV

--- scrape-ancs.js
+++ scrape-ancs.js
@@ -2,25 +2,74 @@
 
 const url = require('url');
 const cheerio = require('cheerio');
+const _ = require('lodash');
+const csvStringify = require('csv-stringify/lib/sync');
 const request = require('./request');
 const ancHomeUrl = 'https://anc.dc.gov/';
 
 request(ancHomeUrl).then(html => cheerio.load(html))
-    .then(process);
+    .then(getAncData)
+    .then(function (data) {
+        console.log(csvStringify(
+            data,
+            {
+                header: true,
+                columns: [
+                    'SMD',
+                    'Name',
+                    'Chair',
+                    'Address',
+                    'Zip',
+                    'Phone',
+                    'Email',
+                ],
+            }
+        ));
+    });
 
-async function process($) {
+async function getAncData($) {
     const links = $('ul.menu-sub > li.leaf > a').get();
+    const data = [];
     for (const a of links) {
         const $a = $(a);
-        const anc = $a.text().trim();
-        let m = anc.match(/^ANC\s+([1-8][A-G])$/);
+        let m = $a.text().trim().match(/^ANC\s+[1-8][A-G]$/);
         if (!m) {
             continue;
         }
         const ancUrl = url.resolve(ancHomeUrl, $a.attr('href'));
         $ = await getCheerio(ancUrl);
-        console.log(anc);
+        const headers = $('table > thead > tr > th')
+            .map((i, th) => $(th).text().trim())
+            .get();
+        const rows = $('table > tbody > tr').get();
+        for (const row of rows) {
+            const values = $(row).find('td')
+                .map((i, th) => $(th).text().trim())
+                .get();
+            const record = _.zipObject(headers, values);
+            if (!record['Name']) {
+                continue;
+            }
+            m = record['Name'].match(/^(.+?)\s+Chairperson$/);
+            if (m) {
+                record['Name'] = m[1];
+                record['Chair'] = 'Y';
+            }
+            else {
+                record['Chair'] = '';
+            }
+            m = record['Address'].match(/^(.*?)\s*Washington,?\s*(?:DC\s*)?(\d+)$/);
+            if (m) {
+                record['Address'] = m[1];
+                record['Zip'] = m[2];
+            }
+            else {
+                record['Zip'] = '';
+            }
+            data.push(record);
+        }
     }
+    return data;
 }
 
 function getCheerio(requestOptions) {
