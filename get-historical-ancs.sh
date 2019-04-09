bea11fb7726b0cedef2d1a754eba8b3c3f576f02
Keith C. Ivey <keith@iveys.org>
2019-04-09 11:47:05 -0400

    rename files dealing with historical, not current, ANCs

--- get-historical-ancs.sh
+++ get-historical-ancs.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 wget -O cache/historical-anc-list.pdf http://www.dcboee.org/pdf_files/nr_849.pdf
 pdftotext -layout cache/historical-anc-list.pdf
-node parse-ancs.js cache/historical-anc-list.txt
+./parse-historical-ancs.js cache/historical-anc-list.txt
